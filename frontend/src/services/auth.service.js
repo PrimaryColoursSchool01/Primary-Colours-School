ad9a284032f5481d4cddde269ca55af97e91b161
Ahmad Ibrahim <ahmadibrahimsearcher@gmail.com>
2026-04-18 23:22:54 +0100

    feat: add forgot password and reset password functionality to auth service

--- frontend/src/services/auth.service.js
+++ frontend/src/services/auth.service.js
@@ -28,3 +28,16 @@ export const changePassword = async (currentPassword, newPassword) => {
   });
   return data;
 };
+
+export const forgotPassword = async (email) => {
+  const { data } = await api.post("/auth/forgot-password", { email });
+  return data;
+};
+
+export const resetPassword = async (token, newPassword) => {
+  const { data } = await api.post("/auth/reset-password", {
+    token,
+    newPassword,
+  });
+  return data;
+};
